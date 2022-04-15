70082a90ba06a8b859dda2c65b816f4941612955
Pradeep <pradeepv0063@gmail.com>
2022-04-15 12:53:22 +0530

    added new button and first screen

---
+++ DIY/Views/NavButton.swift
@@ -0,0 +1,69 @@
+//
+//  NavButton.swift
+//  DIY
+//
+//  Created by Ram Voleti on 15/04/22.
+//
+
+import UIKit
+
+@IBDesignable
+class NavButton: UIButton {
+    
+    var isNextBtn2: Bool = false
+    @IBInspectable var isNextBtn: Bool {
+        set {
+            isNextBtn2 = newValue
+            customInit()
+        }
+        
+        get {
+            return isNextBtn2
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        customInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        customInit()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        customInit()
+    }
+    
+    func customInit() {
+        
+        var title: String
+        var imageName: String
+        var placement: NSDirectionalRectEdge
+        if isNextBtn {
+            title = "Next"
+            imageName = "arrow.right"
+            placement = .trailing
+        } else {
+            title = "Back 12"
+            imageName = "arrow.left"
+            placement = .leading
+        }
+        
+        var container = AttributeContainer()
+        container.font = UIFont.boldSystemFont(ofSize: 20)
+        
+        var config = UIButton.Configuration.filled()
+        config.cornerStyle = .capsule
+        config.buttonSize = .large
+        config.attributedTitle = AttributedString(title, attributes: container)
+        config.image = UIImage(systemName: imageName)
+        config.imagePadding = 10
+        config.imagePlacement = placement
+        config.cornerStyle = .large
+        config.buttonSize = .large
+
+        configuration = config
+    }
+}
